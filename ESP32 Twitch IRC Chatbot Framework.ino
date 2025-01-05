786a364c120c6ee6d3b89ed15a7269bd3e45f627
Jalopy-Jones <Jalopyjones1@gmail.com>
2025-01-05 12:09:14 -0500

    Update ESP32 Twitch IRC Chatbot Framework.ino

--- ESP32 Twitch IRC Chatbot Framework.ino
+++ ESP32 Twitch IRC Chatbot Framework.ino
@@ -1,3 +1,20 @@
+/* ESP32 Twitch IRC Chatbot Framework.ino is a program to connect
+    an ESP32 to Twitch API and Twitch IRC.
+    
+    Copyright (C) 2025  Jalopy Jones
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+  */
+
+
 // THIS PROGRAM REQUIRES THE USE OF THE CREDENTIALS FILE
 
 #include <WiFi.h>
